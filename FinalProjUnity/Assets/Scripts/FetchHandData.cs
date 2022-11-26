161a0682e2c345eb0294f996cfbbb5656518161a
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-27 01:35:36 +0900

    Unity: add feature to frequently fetch hand coord data from server

--- FinalProjUnity/Assets/Scripts/FetchHandData.cs
+++ FinalProjUnity/Assets/Scripts/FetchHandData.cs
@@ -7,15 +7,26 @@ using MiniJSON;
 
 public class FetchHandData : MonoBehaviour
 {
+    public float frequency = 10f;
     public double handX;
     public double handY;
     public double handZ;
     public int handType;
     private string URI = "http://localhost:8000/hand";
     // private string URI = "http://localhost:8000/";
+    private bool isDelay;
+
     void Start()
     {
-        StartCoroutine(GetRequest(this.URI));
+    }
+
+    void Update()
+    {
+        if(!isDelay)
+        {
+            isDelay = true;
+            StartCoroutine(GetRequest(this.URI));
+        }
     }
 
     private IEnumerator GetRequest(string uri)
@@ -31,15 +42,14 @@ public class FetchHandData : MonoBehaviour
             else
             {
                 Debug.Log("Get Request Success");
-                Debug.Log(request.downloadHandler.text);
                 Dictionary<string, object> response = Json.Deserialize(request.downloadHandler.text) as Dictionary<string, object>;
-                Debug.Log(response["hand_coor"]);
 
                 handX  = Convert.ToDouble(((List<object>) response["hand_coor"])[0]);
                 handY  = Convert.ToDouble(((List<object>) response["hand_coor"])[1]);
                 handZ  = Convert.ToDouble(((List<object>) response["hand_coor"])[2]);
-                Debug.Log(response["hand_type"]);
             }
         }
+        yield return new WaitForSeconds(1/frequency);
+        isDelay = false;
     }
 }
